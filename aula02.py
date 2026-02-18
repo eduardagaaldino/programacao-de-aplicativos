34097adba374dc2d0fbdd05ed59f72d42afb93fa
Eduarda Galdino <eduarda.galdino01088169@sesisenaipr.org.br>
2026-02-18 12:45:13 -0300

    primeira aula de programação

---
+++ aula02.py
@@ -0,0 +1,10 @@
+idade = 16 # variavel int
+altura = 1.69 # variavel float
+estou_com_sono = True # variavel booleana
+nome = "Eduarda Galdino" # variavel string
+
+
+print(idade)
+print(altura)
+print(estou_com_sono)
+print(nome)
