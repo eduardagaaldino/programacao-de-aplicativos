fd1acfbd82c361b735110aa2e8209802c3f4c0f3
Aluno <eduarda.paula01088169@sesisenaipr.org.br>
2026-02-20 09:45:28 -0300

    aula input

---
+++ aluno.py
@@ -0,0 +1,10 @@
+nome = (input ("digite seu nome:"))
+idade = int(input("digite seua idade:"))
+telefone = int(input("digite seu telefone"))
+cidade = (input("digite a sua cidade"))
+
+
+print("olá" , nome)
+print("sua idade é:" , idade)
+print("seu telefone é:" , telefone)
+print("você mora em:" , cidade)
---
+++ matematica.py
@@ -0,0 +1,12 @@
+valor1 = int (input ("digite um valor"))
+valor2 = int (input ("digite um valor"))
+
+soma = valor1 + valor2
+subtracao =valor1 - valor2
+divisao = valor1 / valor2
+multiplicacao = valor1 * valor2
+
+print("a soma dos valores é:" , soma)
+print("o resultado da subitração é:" , subtracao)
+print("o divisão dos valores é:" , divisao)
+print("o resultado da multiplicação é" , multiplicacao)
