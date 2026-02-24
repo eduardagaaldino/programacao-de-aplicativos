da6c798f5e98f7cf560110f079d9986e38e91df3
unknown <eduarda.paula01088169@sesisenaipr.org.br>
2026-02-24 11:45:03 -0300

    24/02/2026

---
+++ dolar.py
@@ -0,0 +1,8 @@
+dolar = 5.00
+real = float(input("digite o valor que voce tem:"))
+
+conversao = real / dolar
+
+
+
+print("voce pode comprar" , conversao , "dolares")
---
+++ rpg.py
@@ -0,0 +1,11 @@
+nome = (input("escolha o nome do personagem:"))
+nivel = int(input("escolha o nivel do personagem:"))
+ouro = int(input("escoclha a quantudade de ouro que o personagem carregara:"))
+vida = (input("seu personagem esta vivo ou morto:"))
+
+
+
+print("seu nome é:" , nome)
+print("seu nivel é:" , nivel)
+print("sua quantidae de ouro é:" , ouro)
+print("seu status de vida é;" , vida)
