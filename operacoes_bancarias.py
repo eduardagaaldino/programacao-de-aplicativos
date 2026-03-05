bda129e5a083d7e19501699198ac2ec7944477a9
eduarda <eduarda.paula01088169@sesisenaipr.org.br>
2026-03-05 10:09:36 -0300

    05/03/2026

--- operacoes_bancarias.py
+++ operacoes_bancarias.py
@@ -11,29 +11,28 @@ print("-" * 20)
 escolha = input(" escolha entre 1, 2 ou 3: ")
 
 if escolha == "1":
-    
     valor = float(input("digite o valor desejado : "))
 
     if valor > 0:
-
         valor_total = saldo + valor
         print("o valor total que a em sua conta e de: R$" , valor_total)
 
     else:
         print("deposito invalido!")
 
-if escolha == "2":
+elif escolha == "2":
     valor = float(input("digite o valor desejado : "))
 
     if valor > 0 and (valor <= saldo or valor == 100.00):
-
         valor_total = saldo - valor
         print("o valor total que a em sua conta e de: R$" , valor_total)
 
     else:
         print("saque invalido!")        
 
-if escolha == "3":
+elif escolha == "3":
    valor_total = saldo + 0
    print("o extrato da sua conta e de: R$" , valor_total)
 
+else:
+    print("operacao invalida!")
