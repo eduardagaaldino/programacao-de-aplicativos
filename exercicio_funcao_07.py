874355f288f125e78087158571e18c39a74480d0
unknown <eduardagaaldino@gmail.com>
2026-04-30 10:16:28 -0300

    def ahahahhahahhaaaaaaaaaaaaaaa

--- exercicio_funcao_07.py
+++ exercicio_funcao_07.py
@@ -1,18 +1,17 @@
 print("----------Calculadora de Área de Terrenos----------")
 
-def calcular_area(largura , comprimento):
-    area = largura * comprimento
-    return area
+def calcular_area(largura , comprimento, vezes):
+    while vezes != 3:
+        area = largura * comprimento
+        print(f"a area e de: {area} metros")
+
+        largura = float(input("digite a largura em metros: "))
+        comprimento = float(input("digite o comprimento em metros: "))
+        vezes += 1
+
+print("----------------------------------------------------")
 
 largura = ""
 comprimento = ""
 vezes = 0
-
-while vezes != 3:
-    largura = float(input("digite a largura em metros: "))
-    comprimento = float(input("digite o comprimento em metros: "))
-    area = calcular_area(largura , comprimento)
-    print(f"a area e de: {area} metros")
-    comprimento += 1
-
-print("----------------------------------------------------")
+area = calcular_area(largura , comprimento)
