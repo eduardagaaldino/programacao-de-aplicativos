973be77f9b676a83526b5cc9a58f720e486bc9b1
Aluno <eduardagaaldino@gmail.com>
2026-05-08 08:09:00 -0300

    08/05/2026

--- desafio_crud.py
+++ desafio_crud.py
@@ -2,7 +2,6 @@ print("-------desafio funcao--------")
 
 def adicionar_produto(nome,lista):
     lista.append(nome)
-    print("estoque atual: " , lista)
 
 def listar_produtos(lista):
     for l in lista:
@@ -13,19 +12,20 @@ def atualizar_produto(nome,novo_nome,lista):
     indice = lista.index(nome)
     lista[indice]=novo_nome
 
-def remover_item(indice,lista):
-
-    lista.remove(indice)
+def remover_item(nome,lista):
+    lista.remove(nome)
 
 def exibir_menu(menu):
-    while menu != 5:
+    while menu != 6:
         print("\n-------------------------------------------")
         print("<<<< MENU INICIAL >>>>")
         print("1-adicionar produto")
         print("2-listar produto")
         print("3-atualizar produto")
         print("4-remover produto")
-        print("5-sair")
+        print("5- estoque atual")
+        print("6-sair")
+        print("---------------------------------------------")
         menu = int(input("escolha uma das opcoes a cima: "))
        
         if menu == 1:
@@ -36,11 +36,20 @@ def exibir_menu(menu):
             listar_produtos(estoque)
 
         elif menu == 3:
-            
-            atualizar_produto(nome,novo_nome,lista)
-            
+            removido = input("digite o produto que vc deseja trocar do estoque: ")
+            novo_produto = input("digite o produto que vc deseja adicionar no lugar:")
+            atualizar_produto(removido,novo_produto,estoque)
 
+        elif menu == 4:
+            produto = input("digite o produto que voce deseja remover: ")
+            remover_item(produto,estoque)
 
+        elif menu == 5:
+            print("estoque atual: " , estoque)
+
+        elif menu == 6:
+            print("estoque final: ", estoque)
+            print("programa finalizado!")        
 
 estoque = []
 menu = 0
