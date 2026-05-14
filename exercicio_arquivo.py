89e5de438cfa00da35f05154b7cd498a8fb1040a
unknown <eduardagaaldino@gmail.com>
2026-05-14 10:02:20 -0300

    14/05/2026

--- exercicio_arquivo.py
+++ exercicio_arquivo.py
@@ -8,3 +8,65 @@ def adicionar_destinos():
         arquivo.write(destino + "\n")
     print(f"o destino {destino} foi adicionado!")
 
+def ler_destinos():
+    with open("viagem.txt" , 'r') as arquivo:
+        destinos = arquivo.readlines()
+        
+        posicao = 0
+        for d in destinos:
+            print(f"{d.strip()}({posicao})")
+            posicao += 1
+
+def atualizar_destino():
+    ler_destinos()
+    alterar = int(input("digite o id do destino que vc deseja alterar: "))
+    novo = input("digite o novo destino: ")
+
+    with open("viagem.txt" , 'r') as arquivo:
+        destinos = arquivo.readlines()
+
+    destinos[alterar] = novo + "\n"
+
+    with open("viagem.txt" , 'w') as arquivo:
+        arquivo.writelines(destinos)
+    print("destino atualizado!")
+
+def deletar_destino():
+    ler_destinos()
+    deletar = int(input("digite o id do destino que vc deseja deletar: "))
+    
+    with open("viagem.txt" , 'r') as arquivo:
+        destinos = arquivo.readlines()
+
+    del destinos[deletar] 
+
+    with open("viagem.txt" , 'w') as arquivo:
+        arquivo.writelines(destinos)
+    print("destino removido!") 
+
+
+
+opcao = 0
+
+while opcao != 5:
+    print(25 * "-")
+    print("1- adicionar destino")
+    print("2- listar destinos")
+    print("3- alterar destino")
+    print("4- remover destino")
+    print("5- sair")
+    print(25 * "-")
+    opcao = int(input("escolha uma das opcoes a cima: "))
+
+    if opcao == 1:
+        adicionar_destinos()
+
+    elif opcao == 2:
+        ler_destinos()
+    
+    elif opcao == 3:
+        atualizar_destino()
+
+    elif opcao == 4:
+        deletar_destino()
+print("programa encerrao!")
