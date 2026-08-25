986efc10129c72f7b0cdecdd3b620786c7d0574e
eduardagaaldino <eduardagaaldino@gmil.com>
2026-08-25 12:00:16 -0300

    25/08

--- 3tri-prova_com_assert.py
+++ 3tri-prova_com_assert.py
@@ -230,9 +230,6 @@ def atualizar_laboratorios():
         conexao = sqlite3.connect('laboratirio_exames.db')
         cursor = conexao.cursor()
 
-        id_laboratorio= int(input("Digite o ID do laboratorio que deseja atualizar: "))
-        novo_endereco = input("Digite o novo endereco: ")
-
         sql = f'''
         UPDATE laboratorios
         SET endereco_laboratorio = '{novo_endereco}'
@@ -264,8 +261,6 @@ def excluir_laboratorios():
     try:
         conexao = sqlite3.connect("laboratirio_exames.db")
         cursor = conexao.cursor()
-
-        id_laboratorio = int(input("Digite o ID do laboratorio que deseja excluir: "))
 
         sql = f"DELETE FROM laboratorios WHERE id_laboratorio = {id_laboratorio}"
 
@@ -310,15 +305,18 @@ def menu_laboratorios():
 
             elif opcao == 2:
                 banco = 'laboratirio_exames.db'
-                listar_laboratorios()
+                listar_laboratorios(banco)
             
             elif opcao == 3:
+                id_laboratorio= int(input("Digite o ID do laboratorio que deseja atualizar: "))
+                novo_endereco = input("Digite o novo endereco: ")
                 banco = 'laboratirio_exames.db'
-                atualizar_laboratorios()
+                atualizar_laboratorios(id_laboratorio , novo_endereco , banco)
 
             elif opcao == 4:
+                id_laboratorio = int(input("Digite o ID do laboratorio que deseja excluir: "))
                 banco = 'laboratirio_exames.db'
-                excluir_laboratorios()
+                excluir_laboratorios(id_laboratorio , banco)
 
     except ValueError:
         print("Erro: digite apenas numeros!")
@@ -355,16 +353,16 @@ def menu_principal():
 # menu_principal()
  
 #testes redes
-# assert cadastrar_redes("amora" , "sac", 'laboratirio_exames_teste.db') == "rede de exames cadastrada!"
-# assert listar_redes('laboratirio_exames_teste.db') == "listado com sucesso!"
-# assert atualizar_redes(1 , "abacate" , "sac" , 'laboratirio_exames_teste.db') == "rede atualizada!"
-# assert excluir_redes(2 , 'laboratirio_exames_teste.db') == "rede excluído com sucesso!"
+assert cadastrar_redes("amora" , "sac", 'laboratirio_exames_teste.db') == "rede de exames cadastrada!"
+assert listar_redes('laboratirio_exames_teste.db') == "listado com sucesso!"
+assert atualizar_redes(1 , "abacate" , "sac" , 'laboratirio_exames_teste.db') == "rede atualizada!"
+assert excluir_redes(7 , 'laboratirio_exames_teste.db') == "rede excluído com sucesso!"
 # assert excluir_redes(10 , 'laboratirio_exames_teste.db') == "Nenhuma rede foi encontrada com esse ID."
 
 #testes laboratorios
-assert cadastrar_laboratorios("paranavai" , 1 , 'laboratirio_exames_teste.db') == "laboratorio cadastrado!"
+assert cadastrar_laboratorios("paranavai" , 6 , 'laboratirio_exames_teste.db') == "laboratorio cadastrado!"
 assert listar_laboratorios('laboratirio_exames_teste.db') == "listado com sucesso!"
-assert atualizar_redes(1 , "abacate" , "sac" , 'laboratirio_exames_teste.db') == "laboratorio atualizada!"
-assert excluir_laboratorios(2 , 'laboratirio_exames_teste.db') == "laboratorio excluído com sucesso!"
+assert atualizar_redes(1 , "piracema" , 'laboratirio_exames_teste.db') == "laboratorio atualizada!"
+assert excluir_laboratorios(1 , 'laboratirio_exames_teste.db') == "laboratorio excluído com sucesso!"
 assert excluir_laboratorios(10 , 'laboratirio_exames_teste.db') == "Nenhum laboratorio foi encontrado com esse ID."
 print("testes concluidos!")
